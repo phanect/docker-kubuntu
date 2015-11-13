a56f00c0b223f0fc7cc0966dbe23bd2c9268382e
Jumpei Ogawa <phanective@gmail.com>
2015-11-14 00:45:11 +0900

    Create several versions of Kubuntu image

---
+++ build.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+# set -u is not done here to avoid error by pyvenv bug
+# See: https://github.com/pypa/virtualenv/issues/150
+set -e
+
+pyvenv virtualenv
+source ./virtualenv/bin/activate
+pip install -r requirements.txt
+
+rm -r ./dist
+
+for tag in $(curl -sS https://registry.hub.docker.com/v2/repositories/library/ubuntu/tags/ | jq --raw-output '."results"[]["name"]'); do
+  mkdir --parents ./dist/$tag
+  echo '{ "version": "'$tag'" }' | jinja2 --format=json ./Dockerfile.j2 > dist/$tag/Dockerfile
+done
