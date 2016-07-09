0808d0442094baad4a5bd07868ceb88b0b260d29
Jumpei Ogawa <phanective@gmail.com>
2016-07-10 04:37:52 +0900

    Use mustache to simplify build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,10 @@
 #! /bin/bash
 
-# set -u is not done here to avoid error by pyvenv bug
-# See: https://github.com/pypa/virtualenv/issues/150
-set -e
-
-pyvenv virtualenv
-source ./virtualenv/bin/activate
-pip install -r requirements.txt > /dev/null
+set -eu
 
 rm -rf ./dist
 
 for tag in $(curl -sS https://registry.hub.docker.com/v1/repositories/ubuntu/tags | jq --raw-output '.[]["name"]'); do
   mkdir --parents "./dist/$tag"
-  echo '{ "version": "'$tag'" }' | jinja2 --format=json ./Dockerfile.j2 > "dist/$tag/Dockerfile"
+  echo -e "{ \"version\": \"$tag\" }" | mustache - Dockerfile.mustache > "dist/$tag/Dockerfile"
 done
