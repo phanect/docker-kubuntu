3b617ea76d39bb27172acd632bbe1f8e22737ef7
Jumpei Ogawa <phanective@gmail.com>
2016-07-10 06:50:08 +0900

    Do not include tags with "-"

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ URL="https://registry.hub.docker.com/v2/repositories/library/ubuntu/tags/"
 while [ "$URL" != "null" ]; do
   JSON="$(curl --silent --show-error $URL)"
 
-  for tag in $(echo "$JSON" | jq --raw-output '.results[].name'); do
+  for tag in $(echo "$JSON" | jq --raw-output '.results[].name' | grep --invert-match "-"); do
     mkdir --parents "./dist/$tag"
     echo -e "{ \"version\": \"$tag\" }" | mustache - Dockerfile.mustache > "dist/$tag/Dockerfile"
   done
