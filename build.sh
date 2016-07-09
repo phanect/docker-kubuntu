d6f5350cfab54d5db54f3e8cdbd601b34f6a95e6
Jumpei Ogawa <phanective@gmail.com>
2016-07-10 04:38:35 +0900

    Check if required commands are installed at first in build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 
 set -eu
 
+type jq > /dev/null
+type mustache > /dev/null
+
 rm -rf ./dist
 
 for tag in $(curl -sS https://registry.hub.docker.com/v1/repositories/ubuntu/tags | jq --raw-output '.[]["name"]'); do
