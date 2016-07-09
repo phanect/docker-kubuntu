3af1bbcab23920c0ed0ef33037c94dfafa15b190
Jumpei Ogawa <phanective@gmail.com>
2016-07-10 06:49:48 +0900

    Use Docker Hub API v2

--- build.sh
+++ build.sh
@@ -7,7 +7,15 @@ type mustache > /dev/null
 
 rm --recursive --force ./dist
 
-for tag in $(curl --silent --show-error https://registry.hub.docker.com/v1/repositories/ubuntu/tags | jq --raw-output '.[]["name"]'); do
-  mkdir --parents "./dist/$tag"
-  echo -e "{ \"version\": \"$tag\" }" | mustache - Dockerfile.mustache > "dist/$tag/Dockerfile"
+URL="https://registry.hub.docker.com/v2/repositories/library/ubuntu/tags/"
+
+while [ "$URL" != "null" ]; do
+  JSON="$(curl --silent --show-error $URL)"
+
+  for tag in $(echo "$JSON" | jq --raw-output '.results[].name'); do
+    mkdir --parents "./dist/$tag"
+    echo -e "{ \"version\": \"$tag\" }" | mustache - Dockerfile.mustache > "dist/$tag/Dockerfile"
+  done
+
+  URL="$(echo "$JSON" | jq --raw-output '.["next"]')"
 done
