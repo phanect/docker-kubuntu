869db9598f47a2e49e70bab46e95863a889384a0
Jumpei Ogawa <phanective@gmail.com>
2016-07-10 04:40:21 +0900

    Use long option

--- build.sh
+++ build.sh
@@ -5,9 +5,9 @@ set -eu
 type jq > /dev/null
 type mustache > /dev/null
 
-rm -rf ./dist
+rm --recursive --force ./dist
 
-for tag in $(curl -sS https://registry.hub.docker.com/v1/repositories/ubuntu/tags | jq --raw-output '.[]["name"]'); do
+for tag in $(curl --silent --show-error https://registry.hub.docker.com/v1/repositories/ubuntu/tags | jq --raw-output '.[]["name"]'); do
   mkdir --parents "./dist/$tag"
   echo -e "{ \"version\": \"$tag\" }" | mustache - Dockerfile.mustache > "dist/$tag/Dockerfile"
 done
