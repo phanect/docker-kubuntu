7d7665c3ef76114d09392aa8b0dec9dacd9f832f
Jumpei Ogawa <phanective@gmail.com>
2016-07-10 04:24:21 +0900

    Use jq from distribution On Debian 8 (Jessie), jq 1.4 is available On Ubuntu 16.04 (Xenial), jq 1.5 is available

--- build.sh
+++ build.sh
@@ -10,13 +10,7 @@ pip install -r requirements.txt > /dev/null
 
 rm -rf ./dist
 
-# Install jq 1.5
-# On K/Ubuntu 14.04 only jq 1.3 is available and ."results"[]["name"] is unsupported syntax
-mkdir --parents ./tmp
-curl --location --silent --show-error --output ./tmp/jq https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64
-chmod +x ./tmp/jq
-
-for tag in $(curl -sS https://registry.hub.docker.com/v1/repositories/ubuntu/tags | ./tmp/jq --raw-output '.[]["name"]'); do
+for tag in $(curl -sS https://registry.hub.docker.com/v1/repositories/ubuntu/tags | jq --raw-output '.[]["name"]'); do
   mkdir --parents "./dist/$tag"
   echo '{ "version": "'$tag'" }' | jinja2 --format=json ./Dockerfile.j2 > "dist/$tag/Dockerfile"
 done
